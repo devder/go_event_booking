c9c6ec1c2926cdd15c6d2effdfad902c2377c428
hal <72111003+devder@users.noreply.github.com>
2024-06-15 22:41:09 +0100

    add post route to create new event

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/devder/go_event_booking/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,10 +17,28 @@ func main() {
 	}
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Hello from the API"})
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, gin.H{"events": events})
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	ctx.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
