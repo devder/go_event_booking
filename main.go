d7c0bc6a10da1b4a99d2fc89d303cf523dd1bd01
hal <72111003+devder@users.noreply.github.com>
2024-06-15 11:08:15 +0100

    install gin

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	err := server.SetTrustedProxies(nil)
+	if err != nil {
+		log.Fatalf("Failed to set trusted proxies: %v", err)
+	}
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "Hello from the API"})
+}
