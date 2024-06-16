57b270064e7a75c7e140c70649c17af4f0ca9c74
hal <72111003+devder@users.noreply.github.com>
2024-06-16 17:55:30 +0100

    install jwt

--- main.go
+++ main.go
@@ -6,13 +6,19 @@ import (
 	"github.com/devder/go_event_booking/db"
 	"github.com/devder/go_event_booking/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	db.InitDB()
 	server := gin.Default()
 
-	err := server.SetTrustedProxies(nil)
+	err = server.SetTrustedProxies(nil)
 	if err != nil {
 		log.Fatalf("Failed to set trusted proxies: %v", err)
 	}
